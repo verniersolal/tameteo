962d63056f692235418b576f55a984805bcc416a
EtesseGuillaume <guillaume22etesse@gmail.com>
2018-10-24 17:08:02 +0100

    retrieve form data

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
+function logFormData() {
+    console.log($('form').serializeArray())
+}
+
 function init() {
-    $(document).ready(function(){
+    $(document).ready(function () {
         $('.timepicker').timepicker();
     });
+    var research_button = document.getElementById("research");
+    research_button.addEventListener('click',logFormData);
 }
