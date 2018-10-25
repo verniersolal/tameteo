07ec9a29f52722733bbd38b98888c232e57aeeef
svernier <verniersolal@gmail.com>
2018-10-25 16:55:56 +0200

    create fetch ratp api

--- js/script.js
+++ js/script.js
@@ -1,28 +1,75 @@
-function logFormData() {
+//Sets default values
+var transport = "metros";
+
+// Line Autocomplete
+var lineInput = document.querySelector('#line');
+var stationsInput = document.querySelectorAll('.stations');
+
+function getFormData() {
     console.log($('form').serializeArray());
+    return $('form').serializeArray();
 }
 
-function init() {
-    // Materialize Time picker
-    var elems = document.querySelectorAll('.timepicker');
-    var instances = M.Timepicker.init(elems, {twelveHour: false, i18n: {done: "Valider", cancel: "Annuler"}});
+function getTransport(event) {
+    transport = event.target.id;
+    getLines(transport);
+}
 
-    var research_button = document.getElementById("research");
-    research_button.addEventListener('click', logFormData);
-
-    var getTAMData = "https://api-ratp.pierre-grimaud.fr/v3/lines/metros";
-
-    var sncfCoverageUrl = "https://api.sncf.com/v1/coverage/";
-    var stringAuth = "Basic YzhjNjk0NjMtOGM5Zi00YzllLTk5M2ItNmEyZDdmNzUzODU1Og==";
-
-    fetch(getTAMData, {headers: headers}) // Call the fetch function passing the url of the API as a parameter
+function getLines(transport) {
+    var linesByTransport = {};
+    var url = "https://api-ratp.pierre-grimaud.fr/v3/lines/" + transport;
+    fetch(url) // Call the fetch function passing the url of the API as a parameter
         .then(function (response) {
-            response.text().then(function (value) {
-                console.log(value);
+            response.json().then(function (json) {
+                var lines = json.result[transport];
+                for (var i = 0; i < lines.length; i++) {
+                    linesByTransport[lines[i].code.toString()] = null;
+                }
+                var instances = M.Autocomplete.init(lineInput, {data: linesByTransport});
+                console.log(linesByTransport);
             })
         })
         .catch(function (error) {
             console.log(error)
             // This is where you run code if the server returns any errors
         });
+}
+
+function getStations(transport) {
+    console.log("get Stations");
+    var stationsByLine = {};
+    var line = document.getElementById("line").value;
+    var url = "https://api-ratp.pierre-grimaud.fr/v3/stations/" + transport + "/" + line;
+    fetch(url) // Call the fetch function passing the url of the API as a parameter
+        .then(function (response) {
+            response.json().then(function (json) {
+                var stations = json.result.stations;
+                for(var i=0;i<stations.length;i++){
+                   stationsByLine[stations[i].name] =null;
+                }
+                var instances = M.Autocomplete.init(stationsInput, {data: stationsByLine});
+                console.log(stationsByLine);
+            })
+        })
+        .catch(function (error) {
+            console.log(error)
+            // This is where you run code if the server returns any errors
+        });
+}
+
+function init() {
+    // Materialize Time picker
+    var time_picker = document.querySelectorAll('.timepicker');
+    var instance = M.Timepicker.init(time_picker, {twelveHour: false, i18n: {done: "Valider", cancel: "Annuler"}});
+
+    var research_button = document.getElementById("research");
+    research_button.addEventListener('click', getTransport);
+
+    var transport_buttons = document.getElementsByClassName("rad_btn");
+    for (var i = 0; i < transport_buttons.length; i++) {
+        transport_buttons[i].addEventListener('click', getTransport);
+    }
+
+    document.getElementById("line").addEventListener('change',function (ev) { getStations(transport) })
+
 }
