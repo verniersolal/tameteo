ac9df5c7a0049cb73724bd084dcec7b008bddcb5
svernier <verniersolal@gmail.com>
2018-11-30 11:31:51 +0100

    separate api script files

---
+++ js/weather_api.script.js
@@ -0,0 +1,33 @@
+function displayWeather(json) {
+    var currentTime = new Date().toLocaleTimeString('fr-FR', {
+        hour12: false,
+        hour: "numeric",
+        minute: "numeric"
+    });
+    var city = json.name;
+    var weatherIcon = "icons/" + json.weather[0].icon + ".png";
+    var tempMax = parseInt(json.main.temp_max - 273.15);
+    var tempMin = parseInt(json.main.temp_min - 273.15);
+    var description = json.weather[0].description;
+    document.getElementById('current-time').innerHTML = "Paris, " + currentTime;
+    document.getElementById('weather-icon').src = weatherIcon;
+    document.getElementById('temp-max').innerHTML = tempMax + "°C";
+    document.getElementById('temp-min').innerHTML = tempMin + "°C";
+    document.getElementById('weather-description').innerHTML = description.charAt(0).toUpperCase() + description.slice(1);
+    $('#weather').show();
+}
+
+function getCurrentWeather() {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=Paris,fr?&lang=fr&APPID=0f8facfa615da89b6533605fa0ee2c4d";
+    fetch(url) // Call the fetch function passing the url of the API as a parameter
+        .then(function (response) {
+            response.json().then(function (json) {
+                displayWeather(json);
+            })
+        })
+        .catch(function (error) {
+            // This is where you run code if the server returns any errors
+        });
+}
+
+getCurrentWeather();
